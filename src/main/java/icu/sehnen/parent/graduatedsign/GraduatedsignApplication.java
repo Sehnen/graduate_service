1012244038f0d0bf11ad1f3bd27bdc9a54fc9e21
sehnen <sehnen@126.com>
2021-01-21 22:09:06 +0800

    第一次提交

---
+++ src/main/java/icu/sehnen/parent/graduatedsign/GraduatedsignApplication.java
@@ -0,0 +1,13 @@
+package icu.sehnen.parent.graduatedsign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraduatedsignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraduatedsignApplication.class, args);
+    }
+
+}
---
+++ src/test/java/icu/sehnen/parent/graduatedsign/GraduatedsignApplicationTests.java
@@ -0,0 +1,13 @@
+package icu.sehnen.parent.graduatedsign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraduatedsignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
